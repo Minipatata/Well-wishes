0a389f0919d6e9c29070446e79e9bc9cf9c781e1
Minipatata <ashleynaemmalee3827@ele.cssda.ca>
2025-11-20 19:00:32 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+#Write a program to create a function name well wishes 
+# that will give output hello, how are you!
+def well_wishes(name):
+    print("Hello",name,",how are you!")
+well_wishes("Ashleyna")
